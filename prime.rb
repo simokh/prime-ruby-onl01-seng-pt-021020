ff0466b9f4e42e1fca65c43e550fd04c0deb2831
simokh <m.simokhouili@gmail.com>
2020-04-22 00:04:14 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -6,14 +6,13 @@ require 'bigdecimal/math'
 puts Benchmark.measure { BigMath.PI(10_000) }
 
 
-  def prime?(num)
-    i = 2
-    if num > 1
-    range = (i .. num-1).to_a
-    range.none? do |number|
-      num % number == 0
-    end
-  else
-    false
+def prime?(num)
+  array = (2..num-1).to_a
+  if num > 1
+  array.none? do |number|
+    num % number == 0 
+    end 
+  else 
+  false
   end
 end
